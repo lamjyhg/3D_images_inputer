4efc23cf853d5fb1cd3653364d57d88b8179fb10
jiehong lin <jlin8875@uni.sydney.edu.au>
2023-06-28 18:09:05 +1000

    basic image render

---
+++ src/components/DashBoardGarageCanvas/index.js
@@ -0,0 +1,119 @@
+// React
+import { useEffect, useRef, useState } from 'react';
+import React from 'react';
+
+// Utils
+import { animated } from '@react-spring/three';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import {
+  OrthographicCamera,
+  OrbitControls,
+  TransformControls,
+} from '@react-three/drei';
+import { Canvas, useLoader, extend } from '@react-three/fiber';
+import create from 'zustand';
+
+// Assets
+import hellofont from '../../images/hellofont.json';
+
+// styles
+import './index.scss';
+
+const useStore = create((set) => ({
+  target: null,
+  setTarget: (target) => set({ target }),
+}));
+
+const useCamera = create((set) => ({
+  position: [1000, 800, 1000],
+  setPosition: (position) => set({ position }),
+}));
+extend({ TextGeometry });
+
+const ImageItem = ({ item, handleDelete }) => {
+  const setTarget = useStore((state) => state.setTarget);
+  const colorMap = useLoader(TextureLoader, item.url);
+
+  return (
+    <animated.mesh
+      position={item.position}
+      scale={item.scale}
+      rotation={item.rotation}
+      canvasItemId={item.canvasItemId}
+      onClick={(e) => {
+        setTarget(e.object);
+      }}
+      onDoubleClick={() => {
+        setTarget(null);
+        handleDelete(item.canvasItemId);
+      }}
+    >
+      <boxGeometry />
+      <meshStandardMaterial attach="material" map={colorMap} />
+    </animated.mesh>
+  );
+};
+
+const DashboardGarageCanvas = ({
+  garageName,
+  action,
+  items,
+  handleMove,
+  handleDelete,
+}) => {
+  const group = useRef();
+  const [height, setHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth);
+  const { target, setTarget } = useStore();
+
+  const { position } = useCamera();
+  const font = new FontLoader().parse(hellofont);
+
+  useEffect(() => {
+    setHeight(window.innerHeight);
+  }, [window.innerHeight]);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, [window.innerWidth]);
+
+  return (
+    <div id="garage__canvas">
+      <Canvas
+        style={{
+          background: '#cfcdcd29',
+          height: `${height}px`,
+          width: `${width}px`,
+        }}
+        shadows
+        dpr={[1, 2]}
+        onPointerMissed={(e) => {
+          setTarget(null);
+        }}
+      >
+        <group ref={group}>
+          <ambientLight intensity={0.2} />
+          <directionalLight />
+          <gridHelper args={[100, 100]} position={[0, -40, 0]} />
+          {items.map((item, index) => (
+            <ImageItem
+              item={item}
+              key={index}
+              handleMove={handleMove}
+              handleDelete={handleDelete}
+            ></ImageItem>
+          ))}
+          {target !== null && target != undefined && (
+            <TransformControls object={target} mode={action} />
+          )}
+
+          <OrbitControls makeDefault />
+          <OrthographicCamera makeDefault zoom={2} position={position} />
+        </group>
+      </Canvas>
+    </div>
+  );
+};
+export default DashboardGarageCanvas;
---
+++ src/pages/ShowPage/index.js
@@ -0,0 +1,120 @@
+// React
+import { useEffect, useState } from 'react';
+
+// MUI
+
+// // Components
+// import DashboardGarageCanvas from '../../../components/DashBoardGarageCanvas';
+// import DashboardGarageItemList from '../../../components/DashBoardGarageItemList';
+
+// // Utils
+import Resizer from 'react-image-file-resizer';
+import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+import DashboardGarageCanvas from '../../components/DashBoardGarageCanvas';
+import CanvasHeader from '../../components/CanvasHeader';
+
+// // CSS
+// import './index.scss';
+
+// // Assets
+// import Lottie from 'lottie-react';
+// import emptyImage from './../../../images/lotties/2705-image-loading.json';
+
+function ShowPage() {
+  // header toggle buttons
+  const [action, setAction] = React.useState('translate');
+  const handleChangeAction = (event, newAlignment) => {
+    setAction(newAlignment);
+  };
+  const control = {
+    value: action,
+    onChange: handleChangeAction,
+    exclusive: true,
+  };
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [garageItems, setGarageItems] = useState([]);
+
+  const removeSpace = (string) => {
+    return string.replace(/\s/g, '');
+  };
+
+  const addItemIntoCanvas = (image) => {
+    const id = uuidv4();
+    const newGarageItems = [
+      ...garageItems,
+      {
+        itemId: id,
+        url: URL.createObjectURL(image),
+        canvasItemId: id,
+        position: [0, 1, 0],
+        rotation: [0, 0, 0],
+        scale: [10, 10, 0.1],
+      },
+    ];
+    setGarageItems(newGarageItems);
+  };
+
+  const handleDelete = (id) => {
+    const item = garageItems.find((each) => each.canvasItemId === id);
+    const index = garageItems.indexOf(item);
+    const newList = [...garageItems];
+    newList.splice(index, 1);
+    setGarageItems(newList);
+  };
+
+  const handleMove = (canvasItemId, pos, ro, sc) => {
+    setGarageItems(
+      garageItems.map((item) => {
+        const position =
+          item.canvasItemId === canvasItemId ? pos : item.position;
+        const rotation =
+          item.canvasItemId === canvasItemId ? ro : item.rotation;
+        const scale = item.canvasItemId === canvasItemId ? sc : item.scale;
+        return {
+          ...item,
+          isDragging: true,
+          position,
+          rotation,
+          scale,
+        };
+      })
+    );
+  };
+
+  function save() {
+    // localStorage.setItem('canvas', JSON.stringify(garageItems));
+    // imgData = getBase64Image(bannerImage);
+  }
+  useEffect(() => {
+    // try {
+    //   const garageItemsString = localStorage.getItem('canvas');
+    //   const items = JSON.parse(garageItemsString);
+    //   setGarageItems(items ? items : []);
+    // } catch (error) {}
+  }, []);
+
+  return (
+    <div>
+      <CanvasHeader
+        control={control}
+        addItemIntoCanvas={addItemIntoCanvas}
+        setAction={setAction}
+        save={save}
+      />
+      <DashboardGarageCanvas
+        action={action}
+        items={garageItems}
+        handleMove={handleMove}
+        handleDelete={handleDelete}
+        garageName={undefined}
+      ></DashboardGarageCanvas>
+    </div>
+  );
+}
+export default ShowPage;
+function getBase64Image(bannerImage) {
+  throw new Error('Function not implemented.');
+}
